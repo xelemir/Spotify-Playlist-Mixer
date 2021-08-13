6b002837b7849133a0df23256216f4cfc7dd9211
Jan <68973133+xelemir@users.noreply.github.com>
2021-08-13 20:44:46 +0200

    Create blocked_entities.py

---
+++ blocked_entities.py
@@ -0,0 +1,7 @@
+# Block specific artists by their name, e.g. "Masked Wolf"
+blocked_artists = ["ARTIST_NAME_1>", "<ARTIST_NAME_2>"]
+
+# Block specific tracks by their track id, e.g. "3Ofmpyhv5UAQ70mENzB277"
+blocked_tracks_id = ["<TRACK_ID_1>", "<TRACK_ID_2>", "<TRACK_ID_3>"]
+
+# Leave both empty if you like any song by any artist in your playlist
