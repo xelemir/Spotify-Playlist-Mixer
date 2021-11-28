92b9429ef1fb7caf9628d9df486a076853cf694d
Jan <68973133+xelemir@users.noreply.github.com>
2021-11-28 01:35:15 +0100

    Update and rename blocked_entities.py to data.py

--- blocked_entities.py
+++
@@ -1,2 +0,0 @@
-# Block specific tracks by their id, e.g. "3Ofmpyhv5UAQ70mENzB277"
-blocked_tracks = ["TRACK_ID_0", "TRACK_ID_1", "TRACK_ID_2", "TRACK_ID_3", ]
---
+++ data.py
@@ -0,0 +1,28 @@
+# Block specific tracks by their id. These won't be added to the playlist.
+blocked_tracks = ["5fwSHlTEWpluwOM0Sxnh5k",
+                  "1SgUjGzbmmMOsGFTVwQ59L",
+                  "6MZ91tbZxwwqimJffOoLsW",
+                  "3Ofmpyhv5UAQ70mENzB277",
+                  "1NawUUW8GmiRbJ9UkaKGY4",
+                  "37BZB0z9T8Xu7U3e65qxFy",
+                  "2sXf2JdbB2GlNju00kw9WE",
+                  "40uMIn2zJLAQhNXghRjBed",
+                  "1MIGkQxcdAt2lDx6ySpsc5",
+                  "4H0fjEbU9rzkz0Zp1ftPXD",
+                  "4pt5fDVTg5GhEvEtlz9dKk",
+                  "3Wrjm47oTz2sjIgck11l5e",
+                  "776AftMmFFAWUIEAb3lHhw",
+                  "5yHHWrpZhGc6zyHhbHE4rF",
+                  "3Ofmpyhv5UAQ70mENzB277",
+                  "78SCmQ8A7KQSMdkem2SwBP"]
+
+# The playlist ids the script sources its song from (additionally to user's liked and top songs).
+playlist_src = {"Repeat_Rewind": "37i9dQZF1EpCIUOyPHFK4V",
+                "Todays_Top_Hits": "37i9dQZF1DXcBWIGoYBM5M",
+                "RapCaviar": "37i9dQZF1DX0XUsuxWHRQd",
+                "Mega_Hit_Mix": "37i9dQZF1DXbYM3nMM0oPk",
+                "Pop_Sauce": "37i9dQZF1DXaPCIWxzZwR1"}
+
+CLIENT_ID = "81ab159f3e4a4ef8a2247e7d01ac6a5b"
+CLIENT_SECRET = "8ea4f3f8c6ac4aa69889fde67caf2484"
+REDIRECT_URI = "https://jan.gruettefien.com/spotify-redirect.php"
